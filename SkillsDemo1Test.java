b12ada8e5d6eaabaf7ec8ce92920e2572f7377ad
cindy4127 <103284173+cindy4127@users.noreply.github.com>
2022-05-01 21:04:34 -0700

    update to a passing test

--- SkillsDemo1Test.java
+++ SkillsDemo1Test.java
@@ -5,6 +5,6 @@ public class SkillsDemo1Test{
     //test for the factorial function
     @Test
     public void factorial(){
-        assertEquals(720, SkillsDemo1.factorial(8)); //initally fails 
+        assertEquals(720, SkillsDemo1.factorial(6)); // passes test 
     }
 }
